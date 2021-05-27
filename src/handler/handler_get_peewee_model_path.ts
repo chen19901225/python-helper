9fd0a09eb940209fb305e84b842e4da7b143eea0
vagrant <vagrant@localhost.localdomain>
2021-05-27 11:54:36 +0800

    xi

--- src/handler/handler_get_peewee_model_path.ts
+++ src/handler/handler_get_peewee_model_path.ts
@@ -7,92 +7,79 @@ export function get_peewee_model_path(textEditor: vscode.TextEditor, edit: vscod
     let currentLine = textEditor.selection.active.line;
     let lines = text.split(/\r?\n/);
     let result: [number, string] = [-1, ""];
-    let [model_flag, model_line_index, model_text] = search_previous_model_line(lines, currentLine);
-    // let activeEditor = textEditor.selection.active;
-    if (model_flag) {
-        if (model_line_index > result[0]) {
-            result = [model_line_index, model_text]
-        }
-        // textEditor.insertSnippet(new vscode.SnippetString(insert_model), textEditor.selection.active);
-        // return;
-    }
-    let [equal_flag, equal_line_index, equal_text] = search_previous_equal_line(lines, currentLine);
-    if (equal_flag) {
-        if (equal_line_index > result[0]) {
-            result = [equal_line_index, equal_text]
-        }
-        // textEditor.insertSnippet(new vscode.SnippetString(insert_equal), textEditor.selection.active);
-        // return;
-    }
-
-    let [var_flag, var_line_index, var_text] = search_var_line(lines, currentLine);
-    if (var_flag) {
-        if(var_line_index > result[0]) {
-            result = [var_line_index, var_text]
-        }
-        // textEditor.insertSnippet(new vscode.SnippetString(insert_var), textEditor.selection.active);
-        // return;
-    }
-
-    if(result[1].length > 0) {
-        textEditor.insertSnippet(new vscode.SnippetString(result[1]), textEditor.selection.active);
-    }
-
-
-
-}
-
-
-function search_var_line(lines: Array<string>, currentLine: number): [boolean,number, string] {
-    for (let i = currentLine; i >= Math.max(0, currentLine - 100); i--) {
+    for (let i = currentLine; i >= Math.max(0, i - 100); i++) {
         let text = lines[i];
-        let equal_index = text.indexOf("=")
-        let comma_index = text.indexOf(":")
-        if (equal_index > -1 && comma_index > -1 && comma_index < equal_index) {
-            let part = text.trim().split("=")[0].split(":")[1].trim()
-            return [true,i, part];
+        let [model_flag, model_text] = search_previous_model_line(text);
+        if (model_flag) {
+            textEditor.insertSnippet(new vscode.SnippetString(model_text), textEditor.selection.active);
+            return;
+        }
+        let [equal_flag, equal_text] = search_previous_equal_line(text);
+        if (equal_flag) {
+            textEditor.insertSnippet(new vscode.SnippetString(equal_text), textEditor.selection.active);
+            return;
+        }
+
+        let [var_flag, var_text] = search_var_line(text);
+        if (var_flag) {
+            textEditor.insertSnippet(new vscode.SnippetString(var_text), textEditor.selection.active);
+            return;
+            // return;
         }
     }
-    return [false, -1, ""]
+
+
+
+
+
+}
+
+
+function search_var_line(text: string): [boolean, string] {
+    let equal_index = text.indexOf("=")
+    let comma_index = text.indexOf(":")
+    if (equal_index > -1 && comma_index > -1 && comma_index < equal_index) {
+        let part = text.trim().split("=")[0].split(":")[1].trim()
+        if (part.endsWith("]") && part.indexOf("[") > -1) {
+            part = part.split("[")[1].split("]")[0].trim();
+        }
+        return [true, part];
+    }
+    return [false, ""]
 }
 
 
 
-function search_previous_model_line(lines: Array<string>, currentLine: number): [boolean, number, string] {
+function search_previous_model_line(text: string): [boolean, string] {
     let endList: Array<string> = [".select_with_expression(", "._get_or_raise(", ".get_or_none("]
-    for (let i = currentLine; i >= Math.max(0, currentLine - 100); i--) {
-        let text = lines[i];
-        for (let endStr of endList) {
-            if (text.endsWith(endStr)) {
-                let ele = text.slice(0, text.length - endStr.length);
-                let stack: Array<string> = []
-                for (let j = ele.length - 1; j >= 0; j--) {
-                    let j_ch = ele[j];
-                    if (j_ch == "." || /[_0-9a-zA-Z]/.test(j_ch)) {
-                        stack.unshift(j_ch)
-                        continue
-                    }
-                    break
+    for (let endStr of endList) {
+        if (text.endsWith(endStr)) {
+            let ele = text.slice(0, text.length - endStr.length);
+            let stack: Array<string> = []
+            for (let j = ele.length - 1; j >= 0; j--) {
+                let j_ch = ele[j];
+                if (j_ch == "." || /[_0-9a-zA-Z]/.test(j_ch)) {
+                    stack.unshift(j_ch)
+                    continue
                 }
-                let var_str = stack.join("").trim()
-                // let var_arr = var_str.split(".")
-                return [true, i, var_str]
+                break
             }
+            let var_str = stack.join("").trim()
+            // let var_arr = var_str.split(".")
+            return [true, var_str]
         }
-
     }
-    return [false, -1, ""]
+
+
+    return [false, ""]
 }
 
-function search_previous_equal_line(lines: Array<string>, currentLine: number): [boolean, number, string] {
-    for (let i = currentLine; i >= Math.max(0, currentLine - 100); i--) {
-        let text = lines[i];
-        if (text.indexOf("==") > -1) {
-            let firstVar = text.split("==")[0].trim();
-            let pieces = firstVar.split(".")
-            return [true, i, pieces.slice(0, pieces.length - 1).join(".")];
+function search_previous_equal_line(text: string): [boolean, string] {
+    if (text.indexOf("==") > -1) {
+        let firstVar = text.split("==")[0].trim();
+        let pieces = firstVar.split(".")
+        return [true, pieces.slice(0, pieces.length - 1).join(".")];
 
-        }
     }
-    return [false, -1, ""]
+    return [false, ""]
 }
